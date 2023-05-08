858150a66f94264f9221a55cc1e7444de0ab00ef
mesutilica <mesut.ilica@gmail.com>
2023-05-08 23:03:11 +0300

    8-05-2023 marka düzenleme

---
+++ P013EStore.MVCUI/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.MVCUI.Utils;
+using P013EStore.Service.Abstract;
+
+namespace P013EStore.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly IService<Brand> _service; // readonly nesneler sadece constructor metotta doldurulabilir
+        public BrandsController(IService<Brand> service)
+        {
+            _service = service;
+        }
+
+        // GET: BrandsController
+        public ActionResult Index()
+        {
+            var model = _service.GetAll();
+            return View(model);
+        }
+
+        // GET: BrandsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BrandsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Brand collection, IFormFile? Logo)
+        {
+            try
+            {
+                if (Logo is not null)
+                {
+                    collection.Logo = await FileHelper.FileLoaderAsync(Logo);
+                }
+                await _service.AddAsync(collection);
+                await _service.SaveAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null) // id gönderilmeden direk edit sayfası açılırsa
+            {
+                return BadRequest(); // geriye geçersiz istek hatası dön
+            }
+            var model = await _service.FindAsync(id.Value); // yukardaki id yi ? ile nullable yaparsak
+            if (model == null)
+            {
+                return NotFound(); // kayıt bulunamadı : 404
+            }
+            return View(model);
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Brand collection, IFormFile? Logo, bool? resmiSil)
+        {
+            try
+            {
+                if (resmiSil is not null && resmiSil == true)
+                {
+                    collection.Logo = "";
+                }
+                if (Logo is not null)
+                {
+                    collection.Logo = await FileHelper.FileLoaderAsync(Logo);
+                }
+                _service.Update(collection);
+                await _service.SaveAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ P013EStore.MVCUI/Utils/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace P013EStore.MVCUI.Utils
+{
+    public class FileHelper
+    {
+        public static async Task<string> FileLoaderAsync(IFormFile formFile, string filePath = "/Img/")
+        {
+            string fileName = "";
+
+            fileName = formFile.FileName;
+            string directory = Directory.GetCurrentDirectory() + "/wwwroot" + filePath + fileName;
+            using var stream = new FileStream(directory, FileMode.Create);
+            await formFile.CopyToAsync(stream);
+            return fileName;
+        }
+    }
+}
