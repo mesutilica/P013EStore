016052252940da2de6af9867eec39c0495be97b7
Mesut ILICA <mesutilica@outlook.com>
2023-05-30 23:07:16 +0300

    30-05-2023 proje bitti

--- P013EStore.MVCUI/Controllers/AccountController.cs
+++ P013EStore.MVCUI/Controllers/AccountController.cs
@@ -14,22 +14,81 @@ namespace P013EStore.MVCUI.Controllers
             _service = service;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            var userId = HttpContext.Session.GetInt32("userId");
+            if (userId is null)
+            {
+                TempData["Message"] = "<div class='alert alert-danger'>Lütfen Giriş Yapınız!</div>";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                var user = await _service.GetAsync(u => u.Id == userId);
+                return View(user);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateUserAsync(AppUser appUser)
+        {
+            try
+            {
+                var userId = HttpContext.Session.GetInt32("userId");
+                var user = await _service.GetAsync(u => u.Id == userId);
+                if (user is not null)
+                {
+                    user.Name = appUser.Name;
+                    user.Surname = appUser.Surname;
+                    user.Email = appUser.Email;
+                    user.Phone = appUser.Phone;
+                    user.Password = appUser.Password;                    
+                    _service.Update(user);
+                    await _service.SaveAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+            return View("Index", appUser);
         }
         public IActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Login(UserLoginViewModel viewModel)
+        public async Task<IActionResult> LoginAsync(UserLoginViewModel viewModel)
         {
+            var user = await _service.GetAsync(x => x.Email == viewModel.Email && x.Password == viewModel.Password && x.IsActive);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Giriş Başarısız!");
+            }
+            else
+            {
+                HttpContext.Session.SetInt32("userId", user.Id);
+                HttpContext.Session.SetString("userGuid", user.UserGuid.ToString());
+                return RedirectToAction(nameof(Index));
+            }
             return View();
         }
         public IActionResult SignIn()
         {
             return View();
+        }
+        public IActionResult Logout()
+        {
+            try
+            {
+                HttpContext.Session.Remove("userId");
+                HttpContext.Session.Remove("userGuid");
+            }
+            catch
+            {
+                HttpContext.Session.Clear();
+            }
+            return RedirectToAction("Index", "Home");
         }
         [HttpPost]
         public async Task<IActionResult> SignInAsync(AppUser appUser)
@@ -49,6 +108,7 @@ namespace P013EStore.MVCUI.Controllers
                     appUser.IsAdmin = false;
                     await _service.AddAsync(appUser);
                     await _service.SaveAsync();
+                    TempData["Message"] = "<div class='alert alert-success'>Kayıt Başarılı! Giriş Yapabilirsiniz..</div>";
                     return RedirectToAction(nameof(Login));
                 }
             }
--- P013EStore.MVCUI/Program.cs
+++ P013EStore.MVCUI/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(); // uygulamada session kullanabilmek için
+
 builder.Services.AddDbContext<DatabaseContext>();
 
 builder.Services.AddTransient(typeof(IService<>), typeof(Service<>)); // kendi yazdýðýmýz db iþlemlerini yapan servisi .net core da bu þekilde mvc projesine servis olarak tanýtýyoruz ki kullanabilelim.
@@ -40,6 +42,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession(); // session için
 
 app.UseAuthentication(); // Dikkat! önce UseAuthentication satýrý gelmeli sonra UseAuthorization
 app.UseAuthorization();
