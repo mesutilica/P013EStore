d3723ed36e7ac693f64449daedb8fb79af9fb41a
Mesut ILICA <mesutilica@outlook.com>
2023-05-30 21:16:42 +0300

    30-05-23 account

--- P013EStore.Core/Entities/AppUser.cs
+++ P013EStore.Core/Entities/AppUser.cs
@@ -5,12 +5,13 @@ namespace P013EStore.Core.Entities
     public class AppUser : IEntity
     {
         public int Id { get; set; }
-        [Display(Name = "Ad")]
+        [Display(Name = "Ad"), Required(ErrorMessage = "{0} Alanı Gereklidir!")]
         public string Name { get; set; }
         [Display(Name = "Soyad")]
         public string? Surname { get; set; }
+        [Required(ErrorMessage = "{0} Alanı Gereklidir!")]
         public string Email { get; set; }
-        [Display(Name = "Şifre")]
+        [Display(Name = "Şifre"), Required(ErrorMessage = "{0} Alanı Gereklidir!")]
         public string Password { get; set; }
         [Display(Name = "Kullanıcı Adı")]
         public string? UserName { get; set; }
@@ -23,6 +24,6 @@ namespace P013EStore.Core.Entities
         [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
         public DateTime? CreateDate { get; set; } = DateTime.Now;
         [ScaffoldColumn(false)]
-        public Guid? UserGuid { get; set; }
+        public Guid? UserGuid { get; set; } // bu guid i session veya cookie de saklayarak kullanıcıyı tanımak için kullanırız
     }
 }
--- P013EStore.MVCUI/Controllers/AccountController.cs
+++ P013EStore.MVCUI/Controllers/AccountController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using P013EStore.Core.Entities;
 using P013EStore.MVCUI.Models;
+using P013EStore.Service.Abstract;
 
 namespace P013EStore.MVCUI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IService<AppUser> _service;
+
+        public AccountController(IService<AppUser> service)
+        {
+            _service = service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -24,7 +32,33 @@ namespace P013EStore.MVCUI.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult SignIn(AppUser appUser)
+        public async Task<IActionResult> SignInAsync(AppUser appUser)
+        {
+            try
+            {
+                var kullanici = await _service.GetAsync(x => x.Email == appUser.Email);
+                if (kullanici != null)
+                {
+                    ModelState.AddModelError("", "Bu Mail İle Daha Önce Kayıt Olunmuş!");
+                    return View();
+                }
+                else
+                {
+                    appUser.UserGuid = Guid.NewGuid();
+                    appUser.IsActive = true;
+                    appUser.IsAdmin = false;
+                    await _service.AddAsync(appUser);
+                    await _service.SaveAsync();
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+            return View(appUser);
+        }
+        public IActionResult NewPassword()
         {
             return View();
         }
