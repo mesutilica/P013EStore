aacb5f04e772195572e3353fa8a921fcd6c3ff2e
Mesut ILICA <mesutilica@outlook.com>
2023-06-13 23:05:16 +0300

    13-06-2023 ayarlar

---
+++ P013EStore.MVCUI/Areas/Admin/Controllers/SettingsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.MVCUI.Utils;
+using P013EStore.Service.Abstract;
+
+namespace P013EStore.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Policy = "AdminPolicy")]
+    public class SettingsController : Controller
+    {
+        private readonly IService<Setting> _service;
+
+        public SettingsController(IService<Setting> service)
+        {
+            _service = service;
+        }
+
+        // GET: SettingsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SettingsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SettingsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SettingsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SettingsController/Edit/5
+        public async Task<ActionResult> EditAsync(int? id)
+        {
+            if (id is not null)
+            {
+                return View(await _service.FindAsync(id.Value));
+            }
+            return View();
+        }
+
+        // POST: SettingsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Setting collection, IFormFile? Favicon, IFormFile? Logo)
+        {
+            try
+            {
+                if (Logo is not null)
+                {
+                    collection.Logo = await FileHelper.FileLoaderAsync(Logo);
+                }
+                _service.Update(collection);
+                await _service.SaveAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SettingsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SettingsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ P013EStore.MVCUI/Controllers/NewsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.Service.Abstract;
+
+namespace P013EStore.MVCUI.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly IService<News> _serviceNews;
+
+        public NewsController(IService<News> serviceNews)
+        {
+            _serviceNews = serviceNews;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _serviceNews.GetAllAsync(n => n.IsActive));
+        }
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var model = await _serviceNews.FindAsync(id.Value);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+    }
+}
